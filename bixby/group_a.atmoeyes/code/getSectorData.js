7f78698a5b2a7ffe34beb04930624e1603abbbde
emanuelemessina <emanuelemessina.em@gmail.com>
2022-01-19 17:53:41 +0100

    improved bixby ux and translation

--- bixby/group_a.atmoeyes/code/getSectorData.js
+++ bixby/group_a.atmoeyes/code/getSectorData.js
@@ -5,6 +5,6 @@ var console = require('console');
 module.exports.function = function getSectorData (sectorNumber) {
   var json = http.getUrl(config.get('backend.url')+'/data/aqi/'+sectorNumber, {format: 'json'});
   var node = json.data[0]
-  var output = node ? node.value : null;
+  var output = node ? node.value : -1;
   return output;
 }
